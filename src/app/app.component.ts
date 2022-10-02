ff5c9b76600087d5728afb0ccc534fc490480201
Muhammad Umair <108808568+UmairBaig95@users.noreply.github.com>
2022-10-03 00:03:06 +0500

    update commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,101 @@
 import { Component } from '@angular/core';
-
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
+import { JSONPlaceholderService } from './service/jsonplaceholder.service';
+import axios from 'axios';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'task-task';
+  data?: Array<any>;
+  url?: String;
+  httpClient: any;
+  postId: any;
+  postData?: Array<any>;
+  showForm?: boolean;
+  id?: number;
+  title?: String;
+  body?: String;
+  closeResult?: String;
+  constructor(private modalService: NgbModal) {
+    this.data = new Array<any>();
+    this.url = 'https://jsonplaceholder.typicode.com/posts';
+  }
+
+  openModal(content: any, data: any) {
+    this.id = data.id;
+    this.title = data.title;
+    this.body = data.body;
+    this.modalService
+      .open(content, { ariaLabelledBy: 'modal-basic-title' })
+      .result.then(
+        (result) => {
+          this.closeResult = `Closed with: ${result}`;
+        },
+        (reason) => {
+          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        }
+      );
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+  getDataFromAPI() {
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => {
+        this.data = response.data;
+        this.showForm = false;
+      })
+      .catch((e) => {});
+  }
+  postDataOfForm() {
+    console.log(this.body, this.title);
+    axios.post(`https://jsonplaceholder.typicode.com/posts`, {
+      title: this.title,
+      body: this.body,
+    });
+    this.title = '';
+    this.body = '';
+    alert("Posted")
+  }
+
+  getPostById() {
+    let id: number = 1;
+    let endPoints = '/posts/' + id;
+    this.postData = new Array<any>();
+    axios
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((response) => {
+        this.data = [];
+        this.showForm = true;
+        this.postData = response.data;
+        console.log(this.postData);
+      });
+  }
+
+  DeletePost(id: number) {
+    axios
+      .delete(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then((response) => {
+        alert('Deleted');
+      });
+  }
+  updateData() {
+    axios.put(`https://jsonplaceholder.typicode.com/posts/${this.id}`, {
+      title: this.title,
+      body: this.body,
+    });
+    this.title = '';
+    this.body = '';
+    alert('Data Updated');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
+import {HttpClientModule} from '@angular/common/http'
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    NgbModule,
+    FormsModule,    //added here too
+    ReactiveFormsModule //added here too
   ],
   providers: [],
   bootstrap: [AppComponent]
